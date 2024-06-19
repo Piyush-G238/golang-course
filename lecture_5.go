991e31c1fe4598bbf0287e8a6fa317d9467411d5
Piyush-G238 <guptapiyush238@gmail.com>
2024-06-19 23:46:41 +0530

    learned formatted string and multiline strings

--- lecture_5.go
+++ lecture_5.go
@@ -1,1 +1,24 @@
 /* formatting texts in go programming*/
+
+package main
+
+import "fmt"
+
+func main() {
+	futureValue := 12.345678
+	realFutureValue := 13.9486464
+
+	formattedFV := fmt.Sprintf("Future value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future value (adjusted for inflation): %.1f\n", realFutureValue)
+
+	fmt.Print(formattedFV)
+	fmt.Print(formattedRFV)
+
+	/*formatting multiline string*/
+	var lang1 string = "java"
+	var lang2 string = "golang"
+
+	fmt.Printf(`Currently I am a %v developer,
+but I want to learn %v also`, lang1, lang2)
+
+}
