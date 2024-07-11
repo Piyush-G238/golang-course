4e308ce83ef25748f3c10d36bec79e4ac33df21c
Piyush-G238 <guptapiyush238@gmail.com>
2024-07-12 00:42:55 +0530

    introduction to goroutine and channel

--- app.go
+++ app.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string) {
+	fmt.Println("Hello!", phrase)
+}
+
+func slowGreet(phrase string, doneChan chan bool) {
+	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
+	fmt.Println("Hello!", phrase)
+	// sending data to channel
+	doneChan <- true
+}
 
 func main() {
-	fmt.Print("Hello, world");	
+
+	isdone := make(chan bool)
+
+	// go greet("Nice to meet you!")
+	// go greet("How are you?")
+	go slowGreet("How ... are ... you ...?", isdone)
+
+	go greet("I hope you're liking the course!")
+	<-isdone
 }
