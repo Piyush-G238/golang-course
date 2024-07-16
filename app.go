be9c9e00c52d69c4299375f19bb9082b79ced2d4
Piyush-G238 <guptapiyush238@gmail.com>
2024-07-16 23:26:46 +0530

    completed basics of goroutine and channels

--- app.go
+++ app.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
@@ -18,12 +19,22 @@ func slowGreet(phrase string, doneChan chan bool) {
 
 func main() {
 
-	isdone := make(chan bool)
+	// creating an array of channel with initial size of 4
+	dones := make([]chan bool, 4)
 
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
-	go slowGreet("How ... are ... you ...?", isdone)
+	for i := 0; i < 4; i++ {
+		dones[i] = make(chan bool)
+	}
 
-	go greet("I hope you're liking the course!")
-	<-isdone
+	// isdone := make(chan bool)
+
+	go greet("Nice to meet you!", dones[0])
+	go greet("How are you?", dones[1])
+	go slowGreet("How ... are ... you ...?", dones[2])
+
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, channel := range dones {
+		<-channel
+	}
 }
